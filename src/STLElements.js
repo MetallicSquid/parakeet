21bfb2971468e380e6429d0883ae358c934eba87
MetallicSquid <gcmacneil@icloud.com>
2022-06-15 12:19:07 +0100

    Fix: Changes how the model update time string is rendered
    Where it used to use an incrementing timer, it now has a static string and a duration that is calculated as a tooltip when hovering over the string. This prevents the memory leak that was happening with the previous method.

--- src/STLElements.js
+++ src/STLElements.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from "react";
 import {extend, useFrame, useLoader, useThree} from "@react-three/fiber";
 import {STLLoader} from "three/examples/jsm/loaders/STLLoader";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {Checkbox, Typography} from "@mui/material";
+import {Checkbox, Tooltip, Typography} from "@mui/material";
 import {Autorenew, LineAxis} from "@mui/icons-material";
 import {AxesHelper} from "three";
 
@@ -44,43 +44,55 @@ export function Axes(axes) {
 export class TimeSinceUpdate extends React.Component {
     constructor(props) {
         super(props);
+        const date_hours = (new Date(Date.now()).getHours()).toString();
+        const date_minutes = (new Date(Date.now()).getMinutes()).toString();
         this.state = {
-            time: 0,
-            start: Date.now()
+            start: Date.now(),
+            hours: date_hours.length === 1 ? "0" + date_hours : date_hours,
+            minutes: date_minutes.length === 1 ? "0" + date_minutes : date_minutes,
+            date: new Date(Date.now()).toDateString(),
+            duration_string: "0 seconds ago."
         }
     }
 
     render() {
-        this.timer = setInterval(() => {
-            this.setState({
-                time: Date.now() - this.state.start
-            });
-        }, 1000);
+        const calculateDurationString = () => {
+            const time = Date.now() - this.state.start;
+            const seconds = Math.floor(time / 1000);
+            const minutes = Math.floor(time / 60000);
+            const hours = Math.floor(time / 3600000);
 
-        const {time} = this.state;
-        const minutes = Math.floor(time / 60000);
-        const hours = Math.floor(time / 3600000);
-
-        if (minutes < 60) {
-            return (
-                <Typography>Updated {minutes} {(minutes === 1) ? "minute" : "minutes"} ago.</Typography>
-            )
-        } else {
-            return (
-                <Typography>Updated {hours} {(hours === 1) ? "hour" : "hours"} ago.</Typography>
-            )
+            if (seconds < 60) {
+                this.setState({ duration_string: (seconds + (seconds === 1 ? " second " : " seconds ") + "ago.") });
+            } else if (minutes < 60) {
+                this.setState({ duration_string: (minutes + (minutes === 1 ? " minute " : " minutes ") + "ago.") });
+            } else if (hours < 24) {
+                this.setState({ duration_string: (hours + (hours === 1 ? " hour " : " hours ") + "ago.") });
+            } else {
+                this.setState({ duration_string: "More than a day ago." });
+            }
         }
+
+        return (
+            <Tooltip title={this.state.duration_string} onOpen={calculateDurationString}>
+                <Typography>Last updated at {this.state.hours}:{this.state.minutes} on {this.state.date}.</Typography>
+            </Tooltip>
+        )
     }
 }
 
 export function CheckAutoRotate(onChange) {
     return (
-        <Checkbox defaultChecked={true} onChange={onChange} icon={<Autorenew />} checkedIcon={<Autorenew style={{color: "blue"}} />} />
+        <Tooltip title={"Toggle Rotation"}>
+            <Checkbox defaultChecked={true} onChange={onChange} icon={<Autorenew />} checkedIcon={<Autorenew color="primary" />} />
+        </Tooltip>
     )
 }
 
 export function CheckAxes(onChange) {
     return (
-        <Checkbox defaultChecked={true} onChange={onChange} icon={<LineAxis />} checkedIcon={<LineAxis style={{color: "blue"}} />} />
+        <Tooltip title={"Toggle Axes"}>
+            <Checkbox defaultChecked={true} onChange={onChange} icon={<LineAxis />} checkedIcon={<LineAxis color="primary" />} />
+        </Tooltip>
     )
 }
