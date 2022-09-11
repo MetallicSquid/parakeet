ffa1f2deb7a20c7a480c66da4f06ce16195fcc1b
MetallicSquid <gcmacneil@icloud.com>
2022-09-11 20:21:31 +0100

    Feat: Change migrations to add many more tables
    This adds many more tables to the database to accommodate for a (practically) .json-free data storage system that is used by both plume and roost.

---
+++ database/migrations/20220911183550_create-tables.sql
@@ -0,0 +1,99 @@
+CREATE TABLE Models (
+    model_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    creation_date VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    author VARCHAR NOT NULL
+);
+
+CREATE TABLE Parts (
+    part_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    model_id INTEGER NOT NULL,
+    FOREIGN KEY (model_id)
+       REFERENCES Models (model_id)
+);
+
+CREATE TABLE Instances (
+    path VARCHAR NOT NULL PRIMARY KEY,
+    usage INTEGER NOT NULL,
+    age INTEGER AUTO_INCREMENT,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+       REFERENCES Parts (part_id)
+);
+
+CREATE TABLE IntRangeParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value INTEGER NOT NULL,
+    lower INTEGER NOT NULL,
+    upper INTEGER NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+        REFERENCES Parts (part_id)
+);
+
+CREATE TABLE FloatRangeParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value REAL NOT NULL,
+    lower REAL NOT NULL,
+    upper REAL NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+        REFERENCES Parts (part_id)
+);
+
+CREATE TABLE StringParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value VARCHAR NOT NULL,
+    length INTEGER NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+        REFERENCES Parts (part_id)
+);
+
+CREATE TABLE BoolParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value BOOLEAN NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+        REFERENCES Parts (part_id)
+);
+
+CREATE TABLE IntListParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value INTEGER NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+        REFERENCES Parts (part_id)
+);
+
+CREATE TABLE IntListItems (
+    item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    value INTEGER NOT NULL,
+    parameter_id INTEGER NOT NULL,
+    FOREIGN KEY (parameter_id)
+        REFERENCES IntListParameters (parameter_id)
+);
+
+CREATE TABLE FloatListParameters (
+    parameter_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    default_value REAL NOT NULL,
+    part_id INTEGER NOT NULL,
+    FOREIGN KEY (part_id)
+       REFERENCES Parts (part_id)
+);
+
+CREATE TABLE FloatListItems (
+    item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    value REAL NOT NULL,
+    parameter_id INTEGER NOT NULL,
+    FOREIGN KEY (parameter_id)
+        REFERENCES FloatListParameters (parameter_id)
+);
